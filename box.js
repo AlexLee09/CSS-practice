382ddbde2f5773ea7538ebca186aeb7a3e58722d
AlexLee09 <alexandrekmlee@gmail.com>
2024-02-28 21:16:26 +0800

    Initial commit

---
+++ box.js
@@ -0,0 +1,54 @@
+async function functionOne()
+{
+    let btn = document.getElementById("one");
+    console.log(btn.innerHTML);
+
+   btn.innerHTML = parseInt(btn.innerHTML)+1
+//    console.log(btn.innerHTML)
+//    btn.innerHTML = btn.innerHTML+1
+
+}
+
+async function functionTwo()
+{
+    let btn2 = document.getElementById("two");
+    console.log(btn2.innerHTML);
+
+   btn2.innerHTML = parseInt(btn2.innerHTML)+2
+//    console.log(btn.innerHTML)
+//    btn.innerHTML = btn.innerHTML+1
+
+}
+
+async function functionThree()
+{
+    let btn3 = document.getElementById("three");
+    console.log(btn3.innerHTML);
+
+   btn3.innerHTML = parseInt(btn3.innerHTML)+3
+//    console.log(btn.innerHTML)
+//    btn.innerHTML = btn.innerHTML+1
+
+}
+
+async function functionFour()
+{
+    let btn4 = document.getElementById("four");
+    console.log(btn4.innerHTML);
+
+   btn4.innerHTML = parseInt(btn4.innerHTML)+4
+//    console.log(btn.innerHTML)
+//    btn.innerHTML = btn.innerHTML+1
+
+}
+
+async function functionFive()
+{
+    let btn5 = document.getElementById("five");
+    console.log(btn5.innerHTML);
+
+   btn5.innerHTML = parseInt(btn5.innerHTML)+5
+//    console.log(btn.innerHTML)
+//    btn.innerHTML = btn.innerHTML+1
+
+}
